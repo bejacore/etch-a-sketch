b49105b7de955d00545ccf335dd5f39a5f74cd0c
bejacore <bejacore@gmail.com>
2024-06-26 12:06:18 -0500

    Add number of squares button
    This button allows the user to change the number of the squares in the
    grid

--- javascript.js
+++ javascript.js
@@ -2,16 +2,61 @@ const containerDimentions = 800;
 
 const container = document.querySelector('.container');
 
-let numSideSquares = 16;
-let numSquares = numSideSquares ** 2;
+function getGrid(numSideSquares=16) {
+    let numSquares = numSideSquares ** 2;
 
-const squareDimentions = (containerDimentions-2)/numSideSquares;
+    const squareDimentions = (containerDimentions-2)/numSideSquares;
 
-for (let i = 0; i < numSquares; i++) {
-    const square = document.createElement('div');
-    square.setAttribute('class', 'square');
-    square.style.cssText = `height: ${squareDimentions}px; 
-                            width: ${squareDimentions}px;`;
-    container.appendChild(square);
+    for (let i = 0; i < numSquares; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        square.style.cssText = `height: ${squareDimentions}px; 
+                                width: ${squareDimentions}px;`;
+        container.appendChild(square);
+    }
 }
+
+function getSideSquares() {
+    let numSideSquares = prompt('Enter the number of squares per side');
+    numSideSquares = +numSideSquares;
+
+    if (Number.isNaN(numSideSquares) || 
+        numSideSquares > 100 || 
+        numSideSquares === 0) {
+        alert('Please enter a number between 1 and 100');
+        return getSideSquares();
+    }
+
+    return numSideSquares;
+}
+
+function deleteGrid() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        container.removeChild(square);
+    });
+}
+
+function fillSquare() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        });
+    });
+}
+
+getGrid();
+fillSquare();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    let numSideSquares = getSideSquares();
+    deleteGrid();
+    getGrid(numSideSquares);
+    fillSquare();
+});
+
+
+
 
